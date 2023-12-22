ea5aba98c727b8364cdb3c1856b60ada9849d7c8
shashwatsirjam <73041435+shashwatsirjam@users.noreply.github.com>
2023-12-22 23:17:47 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -4,7 +4,6 @@ var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
 // Pause and play the video, and change the button text
-
 function myFunction() {
   if (video.paused) {
     video.play();
@@ -15,27 +14,27 @@ function myFunction() {
   }
 }
 
-//count-down
-
+// count-down
 const daysE1 = document.getElementById("days");
 const hoursE1 = document.getElementById("hours");
 const minsE1 = document.getElementById("mins");
 const secondsE1 = document.getElementById("seconds");
 
-const newYears ="10 oct 2024";
+// Set the target date to the next year
+const currentYear = new Date().getFullYear();
+const nextYear = currentYear + 1;
+const newYears = `10 oct ${nextYear}`;
 
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
-  const totalSeconds =  ( newYearsDate - currentDate )/
-  1000;
-  
-  const days = Math.floor(totalSeconds/ 3600 /24); 
-  const hours = Math.floor(totalSeconds/ 3600) % 24;
-  const mins = Math.floor(totalSeconds/ 60) % 60;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor((totalSeconds / 3600) % 24);
+  const mins = Math.floor((totalSeconds / 60) % 60);
   const seconds = Math.floor(totalSeconds) % 60;
 
-  
   daysE1.innerHTML = days;
   hoursE1.innerHTML = formatTime(hours);
   minsE1.innerHTML = formatTime(mins);
@@ -46,5 +45,5 @@ function formatTime(time) {
   return time < 10 ? `0${time}` : time;
 }
 
-countdown()
-setInterval(countdown ,1000);
+countdown();
+setInterval(countdown, 1000);
