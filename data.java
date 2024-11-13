fac8d7f94662a0485cefe4cb0e2c5b60708a206a
shashwatsirjam <73041435+shashwatsirjam@users.noreply.github.com>
2024-11-14 02:39:21 +0530

    Update data.java

--- data.java
+++ data.java
@@ -1,19 +1,16 @@
-package com.fsdt.service.config;
+# Service Configuration
+service.name=FSDTService
+service.display-name=FSDT Application Service
+service.description=Windows Service for FSDT Application
+service.app-path=C:\\user\\shaswat
+service.jar-name=FSDT.jar
+service.java-opts=-Xmx512m -Dlog4j.configurationFile=config/log4j2.xml -Djavax.net.ssl.trustStore=cert/176-solace.jks
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+# Logging Configuration
+logging.file.name=${service.app-path}/logs/service-wrapper.log
+logging.level.root=INFO
+logging.level.com.fsdt=DEBUG
 
-@Getter
-@Setter
-@Configuration
-@ConfigurationProperties(prefix = "service")
-public class ServiceConfig {
-    private String name = "FSDTService";
-    private String displayName = "FSDT Application Service";
-    private String description = "Windows Service for FSDT Application";
-    private String appPath = "C:\\user\\shaswat";
-    private String jarName = "FSDT.jar";
-    private String javaOpts = "-Xmx512m -Dlog4j.configurationFile=config/log4j2.xml -Djavax.net.ssl.trustStore=cert/176-solace.jks";
-}
+# Spring Configuration
+spring.main.banner-mode=console
+spring.main.log-startup-info=true
