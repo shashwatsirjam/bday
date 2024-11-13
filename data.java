fd448b2c9a0980652b21947c1b038513392edaf8
shashwatsirjam <73041435+shashwatsirjam@users.noreply.github.com>
2024-11-14 03:19:32 +0530

    Update data.java

--- data.java
+++ data.java
@@ -1,3 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .UseWindowsService();  // This makes the service run as a Windows service
+}
 <Project Sdk="Microsoft.NET.Sdk.Worker">
 
   <PropertyGroup>
